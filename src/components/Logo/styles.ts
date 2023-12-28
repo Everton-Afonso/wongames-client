417e96582b3b6b20267b63be530209634cb96f26
Everton Afonso <evertonafonso0@gmail.com>
2023-12-28 14:02:11 -0300

    feat(Logo): adding mobile and desk styles and validations

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,5 +1,49 @@
 'use client'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+import { LogoProps } from '.'
+
+const wrapperModifiers = {
+  normal: () => css`
+    width: 110px;
+    height: 33px;
+  `,
+
+  large: () => css`
+    width: 200px;
+    height: 59.836px;
+  `,
+
+  white: () => css`
+    color: var(--white);
+  `,
+
+  black: () => css`
+    color: var(--black);
+  `,
+
+  hideOnMobile: () => css`
+    @media screen and (max-width: 768px) {
+      width: 58px;
+      height: 45px;
+
+      svg {
+        height: 45px;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    }
+  `
+}
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ color, size, hideOnMobile }) => css`
+    ${!!color && wrapperModifiers[color]}
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
+  `}
+`
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,9 +1,39 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
+  it('should render a white label by default', () => {
     render(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'color',
+      'var(--white)'
+    )
+  })
+
+  it('should render a black label when color is passed', () => {
+    render(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'color',
+      'var(--black)'
+    )
+  })
+
+  it('should render a normal logo when size is default', () => {
+    render(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '110px'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    render(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '200px'
+    })
   })
 })
