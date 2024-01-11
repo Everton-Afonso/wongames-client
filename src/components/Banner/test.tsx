f40838f98a12317c0743c6a2f3bddb5c5fe528c3
Everton Afonso <evertonafonso0@gmail.com>
2024-01-11 07:56:00 -0300

    feat(Banner): add snapshots

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -12,7 +12,7 @@ const props = {
 
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    render(<Banner {...props} />)
+    const { container } = render(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /defy death/i })
@@ -23,5 +23,7 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
