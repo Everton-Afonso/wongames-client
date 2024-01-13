502c9ede93c8de5650e81d2e363af8af2048f7b1
Everton Afonso <evertonafonso0@gmail.com>
2024-01-13 12:28:11 -0300

    feat(Highlight): adding favicon to Highlight branch

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Highlight = () => {
+  return (
+    <S.WrapperHighlight>
+      <h1>Highlight</h1>
+    </S.WrapperHighlight>
+  )
+}
+
+export default Highlight
