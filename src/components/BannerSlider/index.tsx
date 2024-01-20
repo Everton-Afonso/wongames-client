ae524cfe25a6712de4a8dab97e1fcf82bcd6a263
Everton Afonso <evertonafonso0@gmail.com>
2024-01-20 17:07:37 -0300

    feat(BannerSlider): creation of the slider banner component and its testing

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,42 @@
+import Slider, { SliderSettings } from '../../UI/Slider'
+import Banner, { BannerProps } from '../Banner'
+
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => {
+  const settings: SliderSettings = {
+    dots: true,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+    infinite: true,
+    autoplay: true,
+    pauseOnHover: true,
+    speed: 2000,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          vertical: false,
+          verticalSwiping: false
+        }
+      }
+    ]
+  }
+
+  return (
+    <S.WrapperBannerSlider>
+      <Slider settings={settings}>
+        {items.map((item, index) => (
+          <Banner key={index} {...item} />
+        ))}
+      </Slider>
+    </S.WrapperBannerSlider>
+  )
+}
+
+export default BannerSlider
